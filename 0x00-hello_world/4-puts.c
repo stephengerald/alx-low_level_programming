2407324f233757dc06cebc553cec72eddb0b6b50
Stephen Gerald <stephenokpala11@gmail.com>
2022-09-11 03:50:59 -0700

    new changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/*
+ * File: 4-puts.c
+ * main - Prints "Programming is like building a multilingual puzzle, followed 
+ * by a new line.
+ * Return: Always 0."
+ */
+#include <stdio.h>
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
