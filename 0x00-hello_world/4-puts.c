da5a3674c2d3abe15a5b246429c8a100af0a15ba
Stephen Gerald <stephenokpala11@gmail.com>
2022-09-11 14:25:30 -0700

    new changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-/*
+/**
  * File: 4-puts.c
- * main - Prints "Programming is like building a multilingual puzzle, followed 
- * by a new line.
- * Return: Always 0."
+ * Name: stephen Okpala
  */
 #include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
+ * Return: Always 0."
+ */
 int main(void)
 
 {
