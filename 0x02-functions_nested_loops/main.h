f6750642bf4969fead738ce750d799549dc9197e
Stephen Gerald <stephenokpala11@gmail.com>
2022-09-18 03:48:44 -0700

    new changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+/*
+ * File: main.h
+ * Auth: stephen gerald
+ * Desc: Header file containing declarations for all functions
+ * used in the 0x02-functions_nested_loops directory.
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
