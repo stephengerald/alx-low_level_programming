782eac9112f86cf473098642324a336fe2c2d1cf
Stephen Gerald <stephenokpala11@gmail.com>
2022-10-01 06:44:00 -0700

    new changes

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: argument vector of pointers to strings
  * Return: always 0
  */
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0;
